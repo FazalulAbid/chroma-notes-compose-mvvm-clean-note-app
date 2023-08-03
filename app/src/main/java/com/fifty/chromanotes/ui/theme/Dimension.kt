554892fda60954c105578b03e5248cc5dee3d892
Fazalul Abid <fazalulabid007@gmail.com>
2023-08-03 12:02:08 +0530

    Implemented NotesScreen and NoteItem

---
+++ app/src/main/java/com/fifty/chromanotes/ui/theme/Dimension.kt
@@ -0,0 +1,11 @@
+package com.fifty.chromanotes.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val SizeExtraExtraSmall = 2.dp
+val SizeExtraSmall = 4.dp
+val SizeSmall = 8.dp
+val SizeMedium = 16.dp
+val SizeLarge = 24.dp
+val SizeExtraLarge = 32.dp
+val SizeExtraExtraLarge = 40.dp
